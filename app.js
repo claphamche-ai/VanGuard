c30d4fd36d767aa72f139597a7e82a0ef81d3e9a
claphamche-ai <clapham.che@gmail.com>
2026-04-25 01:17:11 +1200

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const BRAND_NAME = "VanGuard";
 
-if(document.getElementById('page-title')) { document.getElementById('page-title').innerText = `${BRAND_NAME} | Agent v3.1.1`; }
+if(document.getElementById('page-title')) { document.getElementById('page-title').innerText = `${BRAND_NAME} | Agent v3.2.1`; }
 if(document.getElementById('brand-name')) { document.getElementById('brand-name').innerText = BRAND_NAME; }
 
 let timerInterval;
